af1e5857e1f3c528c5c27b93bceaaf3cd4bac680
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-20 22:27:00 +0400

    Starting this project

---
+++ src/main/java/com/CV/Oil_Gas/OilGasApplication.java
@@ -0,0 +1,13 @@
+package com.CV.Oil_Gas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilGasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OilGasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CV/Oil_Gas/OilGasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CV.Oil_Gas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OilGasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
